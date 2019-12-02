799fd8d77a3bf04908e2e215e9a22df51b3c52e8
Alexandre Kahn <alexandrekahn139@gmail.com>
2019-12-02 22:15:40 +0000

    more logical variable name

--- start.js
+++ start.js
@@ -1,17 +1,17 @@
 // temperature-listener.js
 
 const { spawn } = require('child_process');
-const temperatures = []; // Store readings
+const data = []; // Store readings
 console.log("start");
 
 const sensor = spawn('python', ['OralBScanMain.py']);
 sensor.stdout.on('data', function(data) {
 //    console.log(data);
     // Coerce Buffer object to Float
-    temperatures.push(data.toString());
+    data.push(data.toString());
 
     // Log to debug
-   console.log(JSON.parse(temperatures[0]));
+   console.log(JSON.parse(data[0]));
 });
 console.log('ok');
 
